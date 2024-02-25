afc7df56448c80388160250bf20ea71a423886eb
Alex10214 <10214q@gmail.com>
2024-02-25 14:39:48 +0200

    add deals in task 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { CategoriesModule } from './categories/categories.module';
+import { DealsModule } from './deals/deals.module';
 
 @Module({
-  imports: [UserModule, CategoriesModule],
+  imports: [UserModule, CategoriesModule, DealsModule],
   controllers: [AppController],
   providers: [AppService],
 })
